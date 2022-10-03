8cf990b970ef263fae953d561beb79a6e5146469
Laurent Bourgon <laurent.bourgon@outlook.com>
2022-10-03 18:55:36 -0400

    use gen for fibo

--- exercice.py
+++ exercice.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from collections import deque
+from typing import Iterator
 
 
 def get_fibonacci_number(i: int) -> int:
@@ -29,8 +30,19 @@ def get_sorted_dict_by_decimals(d: dict[str, float]) -> dict[str, float]:
     return dict(sorted(d.items(), key=lambda x: x[1] % 1))
 
 
-def fibonacci_numbers(length):
-    pass
+def fibonacci_numbers(n: int) -> Iterator[int]:
+    previous = 0
+    yield previous
+
+    current = 1
+    yield current
+
+    succeeding = 0
+
+    for _ in range(2, n):
+        succeeding = previous + current
+        previous, current = current, succeeding
+        yield current
 
 
 def build_recursive_sequence_generator(TODO):
@@ -47,15 +59,15 @@ if __name__ == "__main__":
     print(get_fibonacci_sequence(10))
     print()
 
-    # spam = {2: 2.1, 3: 3.3, 1: 1.4, 4: 4.2}
-    # eggs = {"foo": 42.6942, "bar": 42.9000, "qux": 69.4269, "yeet": 420.1337}
-    # print(get_sorted_dict_by_decimals(spam))
-    # print(get_sorted_dict_by_decimals(eggs))
-    # print()
+    spam = {2: 2.1, 3: 3.3, 1: 1.4, 4: 4.2}
+    eggs = {"foo": 42.6942, "bar": 42.9000, "qux": 69.4269, "yeet": 420.1337}
+    print(get_sorted_dict_by_decimals(spam))
+    print(get_sorted_dict_by_decimals(eggs))
+    print()
 
-    # for fibo_num in fibonacci_numbers(10):
-    #     print(fibo_num, end=" ")
-    # print("\n")
+    for fibo_num in fibonacci_numbers(10):
+        print(fibo_num, end=" ")
+    print("\n")
 
     # def fibo_def(last_elems):
     #     return last_elems[-1] + last_elems[-2]
